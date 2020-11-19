136b098a4ded99730792cca980dcf34891d1f868
Martin Altamirano <martin.altamirano@proveedor.ues21.edu.ar>
2020-11-19 09:40:08 -0300

    first commit

---
+++ src/main/java/ar/com/workshop/investigador/InvestigadorApplication.java
@@ -0,0 +1,13 @@
+package ar.com.workshop.investigador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestigadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestigadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/workshop/investigador/InvestigadorApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.workshop.investigador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
